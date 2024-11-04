28532a2d2cc1873b0d56d65ba11cc5be1a1f2133
BuildTools <unconfigured@null.spigotmc.org>
2024-11-03 23:55:43 -0500

    update

---
+++ src/utils/Data.js
@@ -0,0 +1,30 @@
+import { FaReact, FaAws, FaPython } from 'react-icons/fa';
+import { SiTensorflow, SiCashapp } from 'react-icons/si';
+
+export const Skills = [
+    {
+        id: 0,
+        tech: 'ReactJS',
+        icon: <FaReact />
+    },
+    {
+        id: 1,
+        tech: 'AWS',
+        icon: <FaAws />
+    },
+    {
+        id: 2,
+        tech: 'Python',
+        icon: <FaPython />
+    },
+    {
+        id: 3,
+        tech: 'Machine Learning',
+        icon: <SiTensorflow /> 
+    },
+    {
+        id: 4,
+        tech: 'Financial Analysis',
+        icon: <SiCashapp />
+    },
+];
