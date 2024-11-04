5eae0aa82a3ecfef5d86bc9e798e02437c3bdbf8
BuildTools <unconfigured@null.spigotmc.org>
2024-11-04 08:02:51 -0500

    updated image file paths

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,9 +1,6 @@
 import { FaReact, FaAws } from 'react-icons/fa';
 import { SiTensorflow, SiCashapp } from 'react-icons/si';
 
-import Project1 from '../assets/project1.png';
-import Project2 from '../assets/project2.png';
-
 export const Skills = [
     {
         id: 0,
@@ -36,7 +33,7 @@ export const projectDetails = [
         project_name: "CFR Kuhn Poker Bot",
         project_desc: "A bot implementing Counterfactual Regret Minimization (CFR) to simulate and optimize strategies for Kuhn Poker. The bot adapts to various opponent types—passive, aggressive, and balanced—while aiming to approximate Nash equilibrium. Features performance tracking and win rate visualizations, making it a powerful tool for studying game theory in simplified poker scenarios.",
         tech_stacks: ['Python', 'CFR Algorithm', 'Game Theory'],
-        project_image: Project1, 
+        project_image: '/Project1.png', 
         project_link: 'https://github.com/A-Dhawan7/CFR_Kuhn_Poker',
         reverse: false
     },
@@ -45,7 +42,7 @@ export const projectDetails = [
         project_name: "ASL Lingo Language Learning App",
         project_desc: "A Duolingo-inspired language-learning platform integrating machine learning for adaptive learning paths and American Sign Language (ASL) recognition. The app offers interactive lessons with real-time tracking and features secure user authentication. Utilizes PyTorch, TensorFlow, and IBM Cloud services for scalable learning experience and computer vision functionalities.",
         tech_stacks: ['React.js', 'PyTorch', 'TensorFlow', 'IBM Cloud', 'Drizzle ORM', 'Clerk'],
-        project_image: Project2, 
+        project_image: '/Project2.png', 
         project_link: 'https://asl-lingoo.vercel.app/',
         reverse: true 
     }
